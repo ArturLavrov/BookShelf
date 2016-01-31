2b92aec515421c86e7f6aaf601a1c874f344fa1d
unknown <arturstylus@gmail.com>
2016-01-31 22:09:07 +0200

    add user setting window,file move method

---
+++ BookShelf.BL/Services/BookServices.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace BookShelf.BL.Services
+{
+    public class BookServices
+    {
+        private void MoveBookToBookShelfFolder(string sourseFile)
+        {
+            string destinationFolder = @"D:\BookShelfStorage";
+            try
+            {
+                System.IO.File.Move(sourseFile, destinationFolder);
+            }
+            catch (System.IO.IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
